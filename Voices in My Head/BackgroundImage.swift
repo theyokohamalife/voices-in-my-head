bef5d8715296c68a9c2c560c36458722bc30406f
LaShawn Toyoda <lts001@gmail.com>
2020-12-21 22:26:51 +0900

    background image fits screen

---
+++ Voices in My Head/BackgroundImage.swift
@@ -0,0 +1,22 @@
+//
+//  BackgroundImage.swift
+//  Voices in My Head
+//
+//  Created by LaShawn on 2020/12/21.
+//
+
+import SwiftUI
+
+struct BackgroundImage: View {
+    var body: some View {
+        Image("ducky")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+struct BackgroundImage_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImage()
+    }
+}
