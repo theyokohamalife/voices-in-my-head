d3c48919ee465ff8892d523aa4c9cc71d834632b
LaShawn Toyoda <lts001@gmail.com>
2020-12-21 21:08:13 +0900

    Initial Commit

---
+++ Voices in My Head/Voices_in_My_HeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voices_in_My_HeadApp.swift
+//  Voices in My Head
+//
+//  Created by LaShawn on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Voices_in_My_HeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
