2abfd9c092738922efbad2cd0e7c059ea177a90e
LaShawn Toyoda <lts001@gmail.com>
2020-12-22 15:48:38 +0900

    basic game layout

--- Voices in My Head/ContentView.swift
+++ Voices in My Head/ContentView.swift
@@ -11,16 +11,50 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Voices in My Head")
-                .padding()
-                .font(.title)
-                .foregroundColor(.black)
+            HStack {
+                Text("HP")
+                    .fontWeight(.bold)
+                    
+                Rectangle()
+                    .frame(width: 200.0, height: 30.0)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
+            }
+            // Enemy
+            Rectangle()
+                .frame(width: 150.0, height: 150.0)
+                .foregroundColor(.gray)
+            Spacer()
+            // Dialogue
+            ZStack {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.gray)
+                    .frame(width: .infinity, height: 200)
+                Text("Game dialogue")
+                    .foregroundColor(.black)
+            }
+            // Abilities
+            HStack {
+                ZStack {
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 80, height:80)
+                    Text("Attack")
+                }
+                ZStack {
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 80, height:80)
+                    Text("Defend")
+                }
+                ZStack {
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 80, height:80)
+                    Text("Heal")
+                }
+            }
             
-//            BackgroundImage() //component
             
-            Text("Let the quest begin")
-                .font(.subheadline)
-                .padding()
         }
     }
 }
