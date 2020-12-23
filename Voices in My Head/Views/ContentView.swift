91013a87fd4b392ba4fe9a02b1333f606ecc0d71
LaShawn Toyoda <lts001@gmail.com>
2020-12-23 17:16:07 +0900

    no build errors

--- Voices in My Head/Views/ContentView.swift
+++ Voices in My Head/Views/ContentView.swift
@@ -19,15 +19,15 @@ struct ContentView: View {
    
     
     // SPEECH START
-//    @IBOutlet weak var lb_speech: UILabel!
-//    @IBOutlet weak var view_color: UIView!
-//    @IBOutlet weak var btn_start: UIButton!
+    @State var lb_speech: UILabel!
+    @State var view_color: UIView!
+    @State var btn_start: UIButton!
     
     let audioEngine = AVAudioEngine()
     let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
     let request = SFSpeechAudioBufferRecognitionRequest()
-    var task : SFSpeechRecognitionTask!
-    var isStart: Bool = false
+    @State var task : SFSpeechRecognitionTask!
+    @State var isStart: Bool = false
     
     
     func requestPermission() {
@@ -50,6 +50,50 @@ struct ContentView: View {
             }
         }
     }
+    
+    func startSpeechRecognition() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {
+            (buffer, _) in self.request.append(buffer)
+        }
+        audioEngine.prepare()
+                do {
+                    try audioEngine.start()
+                } catch let error {
+                    print("Error in the audio =\(error.localizedDescription)")
+                }
+                guard let myRecognization = SFSpeechRecognizer() else {
+                    print("Recognition is not allowed")
+                    return
+                }
+                if !myRecognization.isAvailable {
+                    print("Try again later")
+                }
+                task = speechRecognizer?.recognitionTask(with: request, resultHandler: { (response, error) in
+                    guard let response = response else {
+                        if error != nil {
+                            print(error.debugDescription)
+                        }else {
+                            print("Problem with getting response")
+                        }
+                        return
+                    };
+                    let message = response.bestTranscription.formattedString
+                                print("Message : \(message)")
+                                self.lb_speech.text = message
+                    var lastString: String = ""
+                                for segment in response.bestTranscription.segments {
+                                    let indexTo = message.index(message.startIndex, offsetBy: segment.substringRange.location)
+                                    lastString = String(message[indexTo...])
+                                }
+                                
+                                if lastString == "red" {
+                                    print("red")
+                                    }
+                }
+                )}
     // SPEECH END
     
     var body: some View {
@@ -76,7 +120,9 @@ struct ContentView: View {
                     }
             }
             DialogueView()
-            Button("Command") {playerAttack()}
+            Button("Command") {playerAttack()
+                print("Player Attack")
+            }
                 .frame(width: 100.0, height: 30.0)
                 .foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
                 .background(Color(.systemBlue))
