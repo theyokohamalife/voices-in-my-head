f9788751918e11a1529f98f89435ec8c97f7b319
LaShawn Toyoda <lts001@gmail.com>
2020-12-23 16:41:06 +0900

    permissions not working

--- Voices in My Head/Views/ContentView.swift
+++ Voices in My Head/Views/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import Speech
 
 struct ContentView: View {
     
     @State private var enemyHP: Int = 999
     @State private var playerHP: Int = 130
+    @State private var gameOver: Bool = false
+    @State private var commandBtnEnabled = false
     
     @State var timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect()
+   
+    
+    // SPEECH START
+//    @IBOutlet weak var lb_speech: UILabel!
+//    @IBOutlet weak var view_color: UIView!
+//    @IBOutlet weak var btn_start: UIButton!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var task : SFSpeechRecognitionTask!
+    var isStart: Bool = false
+    
+    
+    func requestPermission() {
+        SFSpeechRecognizer.requestAuthorization{
+            (authState) in
+            OperationQueue.main.addOperation {
+                if authState == .authorized {
+                    print("Permission granted")
+                    self.commandBtnEnabled = true
+                }
+                else if authState == .denied {
+                    print("User denied permission")
+                }
+                else if authState == .notDetermined {
+                    print("Phone doesn't have speech recognition")
+                }
+                else if authState == .restricted {
+                    print("Speech recognition is restricted")
+                }
+            }
+        }
+    }
+    // SPEECH END
     
     var body: some View {
+        
         VStack {
+            
             Spacer()
             
             // Enemy
@@ -43,9 +83,8 @@ struct ContentView: View {
             
         }
     }
-    func playerAttack() {
-        self.enemyHP -= Int.random(in: 20...40)
-    }
+    // Abilities
+    func playerAttack() {self.enemyHP -= Int.random(in: 20...40)}
 }
 
 struct ContentView_Previews: PreviewProvider {
