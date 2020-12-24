322ea4471aebcdd8d8ea576e6ae7c6feedcbb21d
LaShawn Toyoda <lts001@gmail.com>
2020-12-24 11:27:37 +0900

    heal works

--- Voices in My Head/ContentView.swift
+++ Voices in My Head/ContentView.swift
@@ -12,25 +12,34 @@ import Speech
 struct ContentView: View {
     @EnvironmentObject var swiftUISpeech:SwiftUISpeech
     @EnvironmentObject var command:SwiftUISpeech
+    @State var enemyName:String = "Soul Eater"
     @State var enemyHP:Int = 999
-//    @State private var command:String = ""
-    @State private var lastString:String = ""
+    @State var playerHP:Int = 130
+    @State var playerShield:Int = 0
     
-    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    let timer = Timer.publish(every: 8, on: .main, in: .common).autoconnect()
     
     
     var body: some View {
         
         VStack {
-            
+            Text(enemyName)
+                .font(.title)
+                .onReceive(timer, perform: { _ in
+                    enemyAttack()
+                })
             ZStack {
                 Rectangle()
                     .frame(width: 200, height: 30)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 Text("HP: \(enemyHP)")
                     .padding()
+                
             }
+            
             VStack{
+                
                 // prints results to screen
                 Text("\(swiftUISpeech.outputText)")
                     .onChange(of: swiftUISpeech.outputText, perform: { value in
@@ -39,25 +48,25 @@ struct ContentView: View {
                             print("Attack")
                             playerAttack()
                         }
+                        if command.outputText.contains("Heal") || command.outputText.contains("heal") {
+                            print("Heal")
+                            playerHeal()
+                        }
                     })
-//                    .onReceive(timer){ _ in
-//                        if command.outputText == "attack" {
-//                            print("attack")
-//                            playerAttack()
-//                        }
-//                        if command.outputText == "Attack" {
-//                            print("Attack")
-//                            playerAttack()
-//                        }
-//                    }
-//                    .onAppear(perform:{
-                        
-                        
-//                    })
                 
             }.frame(width: 300,height: 300)
             
             VStack {
+                Spacer()
+                ZStack {
+                    
+                    Rectangle()
+                        .frame(width: 200, height: 30)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text("HP: \(playerHP)")
+                        .padding()
+                        
+                }
                 Spacer()
                 // Speech button
                 swiftUISpeech.getButton()
@@ -68,8 +77,29 @@ struct ContentView: View {
         }
         
     }
+    // Player Actions
     func playerAttack() {
-        enemyHP -= Int.random(in: 20...40)
+        enemyHP -= Int.random(in: 40...70)
+        if enemyHP <= 0 {
+            print("YOU WON")
+        }
+    }
+    func playerHeal() {
+        if playerHP < 130 {
+            playerHP += Int.random(in: 40...50)
+        }
+        if playerHP > 130 {
+            playerHP = 130
+        }
+        print("Player heals")
+    }
+    // Enemy Actions
+    func enemyAttack() {
+        print("Enemy attacks")
+        playerHP -= Int.random(in: 10...20)
+        if playerHP <= 0 {
+            print("GAME OVER")
+        }
     }
 }
 
